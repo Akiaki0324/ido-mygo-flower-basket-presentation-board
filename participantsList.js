08358c5f77a775590324c95e2c934f5f95661aa4
Akiaki <akiaki@haguro.moe>
2023-12-27 00:47:32 +0800

    还有名单

---
+++ participantsList.js
@@ -0,0 +1,9 @@
+const participantsList = [
+    '八幡', '青木陽聡', 'かき眠い', '白夜。', 'YKYMK',
+    'Yuuri', '附近的几家店', '无氪Uka', 'kumo', '果力成',
+    '焼き魚', '唐可可一生推', '橼月君峰', '澄空', 'MACO_NOEI',
+    '大塚澪', 'Mozalic', '雨落', 'Takobell', '1ChRN',
+
+]
+
+export default participantsList;
